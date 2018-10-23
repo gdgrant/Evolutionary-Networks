7586f8cc2b775806f3fc347066487f988a40bdf3
gdgrant <gdgrant@uchicago.edu>
2018-10-23 18:00:42 -0500

    Set overnight sweeps

--- run_models.py
+++ run_models.py
@@ -15,10 +15,60 @@ def try_model(updates):
         quit('Quit by KeyboardInterrupt.  Run time: {:5.3f} s.\n\n'.format(time.time()-t0))
 
 
-updates = {
-    'iterations'    : 10001,
-    'task'          : 'dms',
-    'save_fn'       : 'spiking_latent_dms_v0'
+def cross_rate_sweep():
+
+    updates = {
+        'iterations'        : 1001,
+        'task'              : 'dms'
+        'n_hidden'          : 150,
+    }
+
+    for i, rate in enumerate([0.0, 0.01, 0.05, 0.1, 0.2]):
+        updates['cross_rate'] = rate
+        updates['save_fn'] = 'crossrate{}_dms_v0'.format(i)
+        try_model(updates)
+
+
+def mutation_strength_sweep():
+
+    updates = {
+        'iterations'        : 1001,
+        'task'              : 'dms'
+        'n_hidden'          : 150,
+    }
+
+    for i, rate in enumerate([0.1, 0.25, 0.4, 0.6]):
+        updates['mutation_strength'] = rate
+        updates['save_fn'] = 'mutstrength{}_dms_v0'.format(i)
+        try_model(updates)
+
+
+cross_rate_sweep()
+
+double_neurons = {
+    'iterations'      : 10001,
+    'task'            : 'dms',
+    'save_fn'         : 'double_neurons_dms_v0',
+    'n_hidden'        : 200,
 }
 
-try_model(updates)
+output_constant = {
+    'iterations'      : 10001,
+    'task'            : 'dms',
+    'save_fn'         : 'output_constant80_dms_v0',
+    'output_constant' : 80,
+}
+
+base_model = {
+    'iterations'          : 10001,
+    'task'                : 'dms',
+    'save_fn'             : 'momentum_nocross_dms_v0',
+    'output_constant'     : 80,
+    'use_weight_momentum' : True,
+    'momentum_scale'      : 1.,
+    'mutation_rate'       : 0.75,
+    'mutation_strength'   : 0.40,
+    'cross_rate'          : 0.
+}
+
+#try_model(base_model)
