de7bb4313ab3b530e8dfe4b83cbac96519292c83
gdgrant <gdgrant@uchicago.edu>
2018-10-31 16:35:36 -0500

    Finished dtype-aware matmul

--- utils.py
+++ utils.py
@@ -34,12 +34,16 @@ def to_cpu(x):
 
 ### Network functions
 
-def matmul(a, b):
+def matmul(a, b, l=False):
     """ Does matrix multiplication as a @ b, accounting for
-        whether either is of dtype=int8 """
+        whether either is of dtype=int8.  'l' indicates whether
+        this matrix operation is being used for a latency weight matrix """
     if cp.int8 in [a.dtype, b.dtype]:
         # Set up for a=state, b=weights
-        return cp.sum(a[...,cp.newaxis]*b[:,cp.newaxis,...], axis=-2, dtype=cp.float16)
+        if not l:
+            return cp.sum(a[...,cp.newaxis]*b[:,cp.newaxis,...], axis=-2, dtype=cp.float16)
+        else:
+            return cp.sum(a[...,cp.newaxis]*b[:,:,cp.newaxis,...], axis=-2, dtype=cp.float16)
     else:
         return cp.matmul(a, b)
 
